87b3b9e36e760f887b8bd5df7d6b10dcd7cf365a
MastarCheeze <mon10180218@gmail.com>
2024-10-19 18:33:26 +0800

    change current line number to yellow

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,17 @@
+-- theme
 return {
   "navarasu/onedark.nvim",
   lazy = false,
   priority = 1000,
   config = function()
     vim.cmd([[ colorscheme onedark ]])
+
+    local c = require("onedark.colors")
     require("onedark").setup({
       style = "deep",
+      highlights = {
+        CursorLineNr = { fmt = "bold" },
+      },
     })
     require("onedark").load()
   end,
