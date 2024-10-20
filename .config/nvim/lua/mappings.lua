a8f2c57ff58d06335ff3fc3f0ed10a58ee95f5e3
MastarCheeze <mon10180218@gmail.com>
2024-10-20 12:22:13 +0800

    nvim improve mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,20 +1,23 @@
 local map = vim.keymap.set
+local del = vim.keymap.del
 
 -- File explorer
-map("n", "<C-n>", "<cmd>NvimTreeToggle <cr>", { desc = "NvimTree toggle window", silent = true })
-map("n", "<Leader>e", "<cmd>NvimTreeFocus <cr>", { desc = "NvimTree focus window", silent = true })
+map({ "n", "i", "v" }, "<M-n>", "<cmd>NvimTreeToggle <cr>", { desc = "NvimTree toggle window", silent = true })
+map({ "n", "i", "v" }, "<M-e>", "<cmd>NvimTreeFocus <cr>", { desc = "NvimTree focus window", silent = true })
 
 -- Buffers
-map({"n", "i", "v"}, "<M-]>", "<cmd>bn <cr>", { desc = "Buffer next", silent = true })
-map({"n", "i", "v"}, "<M-[>", "<cmd>bp <cr>", { desc = "Buffer prev", silent = true })
-map({"n", "i", "v"}, "<Leader>bq", "<cmd>bn <cr>", { desc = "Buffer close all others", silent = true })
+map({ "n", "i", "v" }, "<M-]>", "<cmd>bn <cr>", { desc = "Buffer next", silent = true })
+map({ "n", "i", "v" }, "<M-[>", "<cmd>bp <cr>", { desc = "Buffer prev", silent = true })
+map({ "n", "i", "v" }, "<M-q>", "<cmd>:BufOnly <cr>", { desc = "Buffer close all others", silent = true })
 
 -- Format
-map("n", "<Leader>fm", function()
-  require("conform").format({ lsp_fallback = true })
+map({ "n", "v" }, ",f", function()
+  require("conform").format({ lsp_format = true })
 end, { desc = "Format file" })
 
 -- Comment
-map("n", "<leader>/", "gcc", { desc = "Toggle comment", remap = true })
-map("v", "<leader>/", "gc", { desc = "Toggle comment", remap = true })
+map("n", ",/", "gcc", { desc = "Toggle comment", remap = true })
+map("v", ",/", "gc", { desc = "Toggle comment", remap = true })
 
+-- WhichKey guide
+map("n", "<Leader>?", "<cmd>WhichKey <cr>", { desc = "WhichKey all keymaps" })
