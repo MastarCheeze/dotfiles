1e3d718d1c2138a975b7177a440a6a20de3cabd3
MastarCheeze <mon10180218@gmail.com>
2024-10-20 12:22:37 +0800

    nvim BufOnly buffer closing plugiin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,3 +1,5 @@
 return {
   "nvim-tree/nvim-web-devicons", -- file explorer icons
+  "vim-scripts/BufOnly.vim", -- command for closing buffers
+  -- { "karb94/neoscroll.nvim", opts = {} }, -- smooth scrolling
 }
