afa93361ba8b95eabeaaa2dedeb8ca40d87be2ad
MastarCheeze <mon10180218@gmail.com>
2024-10-20 15:00:13 +0800

    nvim reduce whichkey options

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -5,13 +5,32 @@ return {
   lazy = false,
   opts = {
     preset = "modern",
-    delay = 600,
+    delay = 1000,
     triggers = {
       { "<auto>", mode = "nixsotc" },
       { "a", mode = { "n", "v" } },
     },
+    plugins = {
+      marks = false, -- shows a list of your marks on ' and `
+      registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+      -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+      -- No actual key bindings are created
+      spelling = {
+        enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+        suggestions = 20, -- how many suggestions should be shown in the list?
+      },
+      presets = {
+        operators = false, -- adds help for operators like d, y, ...
+        motions = false, -- adds help for motions
+        text_objects = false, -- help for text objects triggered after entering an operator
+        windows = true, -- default bindings on <c-w>
+        nav = false, -- misc bindings to work with windows
+        z = true, -- bindings for folds, spelling and others prefixed with z
+        g = true, -- bindings for prefixed with g
+      },
+    },
     win = {
-      width = 80,
+      width = 100,
     },
   },
 }
