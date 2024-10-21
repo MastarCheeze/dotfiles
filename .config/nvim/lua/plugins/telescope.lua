b22a3746928c2193fa2ade20f1f48ffedbef1b7c
MastarCheeze <mon10180218@gmail.com>
2024-10-21 14:46:27 +0800

    config telescope layout and mappings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,13 +3,29 @@ return {
   branch = "0.1.x",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    -- local defaults = require("telescope.themes").get_dropdown({})
-    local defaults = {}
+    local defaults = require("telescope.themes").get_dropdown({})
+    local actions = require("telescope.actions")
+
     defaults.prompt_prefix = " "
+    defaults.layout_strategy = "center"
+    defaults.layout_config = {
+      center = {
+        height = 0.4,
+        anchor = "S",
+      },
+    }
 
     require("telescope").setup({
       defaults = defaults,
       pickers = {
+        buffers = {
+          mappings = {
+            i = {
+              ["<C-w>"] = actions.delete_buffer,
+              ["<C-a>"] = actions.toggle_all,
+            },
+          },
+        },
         git_status = {
           git_icons = {
             added = "+",
