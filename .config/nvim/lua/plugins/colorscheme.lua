c0dd9580c8c1dbca6eb636f0c2931dddd6eb416b
MastarCheeze <mon10180218@gmail.com>
2024-10-22 01:11:37 +0800

    use higher contrast colors for theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,7 +8,18 @@ return {
 
     local c = require("onedark.colors")
     require("onedark").setup({
-      style = "deep",
+      style = "darker",
+      colors = {
+        bg0 = "#14161A",
+        bg1 = "#1D2025",
+        bg2 = "#252A31",
+        bg3 = "#282B34",
+        bg_d = "#16191D",
+        fg = "#CBCFD7",
+        cyan = "#88DCE7",
+        yellow = "#EFDA81",
+        grey = "#40454E",
+      },
       highlights = {
         CursorLineNr = { fmt = "bold" },
       },
