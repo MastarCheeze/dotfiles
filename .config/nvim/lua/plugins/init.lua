584dd6d154c130377dae908577776b50d8abbc89
MastarCheeze <mon10180218@gmail.com>
2024-10-22 22:50:59 +0800

    clean up unused plugins

--- .config/nvim/lua/plugins/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-  { "nvim-tree/nvim-web-devicons", lazy = true }, -- file explorer icons
-  { "vim-scripts/BufOnly.vim", lazy = true, cmd = "BufOnly" }, -- command for closing buffers
-  -- { "karb94/neoscroll.nvim", opts = {} }, -- smooth scrolling
-}
