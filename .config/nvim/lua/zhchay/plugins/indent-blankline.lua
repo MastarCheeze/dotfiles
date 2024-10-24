ec71ee155ea5ad4b41f4f7fcd1b8e797e72345cf
MastarCheeze <mon10180218@gmail.com>
2024-10-24 22:05:03 +0800

    nvim plugin indent-blankline

---
+++ .config/nvim/lua/zhchay/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  event = { "BufNew" },
+  opts = {
+    scope = { show_start = false, show_end = false },
+    exclude = {
+      filetypes = { "dashboard" },
+    },
+  },
+}
+
