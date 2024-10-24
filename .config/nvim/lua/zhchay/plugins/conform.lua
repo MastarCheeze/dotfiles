6c33bf67f884acfe8c44b01cc7b6220bd9616363
MastarCheeze <mon10180218@gmail.com>
2024-10-24 21:41:40 +0800

    nvim plugin conform

---
+++ .config/nvim/lua/zhchay/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre", "BufNewFile" },
+  cmd = { "ConformInfo" },
+
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      c = { "clang-format" },
+      cpp = { "clang-format" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      html = { "prettier" },
+      markdown = { "prettier" },
+    },
+  },
+
+  init = function()
+    local map = vim.keymap.set
+
+    map({ "n", "v" }, "<leader>m", function()
+      require("conform").format({ lsp_format = true })
+    end, { desc = "Format buffer" })
+  end,
+}
