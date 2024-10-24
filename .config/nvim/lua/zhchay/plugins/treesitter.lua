3bdda7ec98daba5153770dcc4454442a9341b6bd
MastarCheeze <mon10180218@gmail.com>
2024-10-24 22:33:52 +0800

    nvim plugin treesitter

---
+++ .config/nvim/lua/zhchay/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufRead", "BufNewFile" },
+  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "help",
+      "c",
+      "cpp",
+      "javascript",
+      "typescript",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "json",
+      "jsonc",
+    },
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
+    indent = { enable = true },
+  },
+}
