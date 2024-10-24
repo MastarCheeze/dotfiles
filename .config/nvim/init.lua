304780311344eca4521179c1e3177875e1ef709b
MastarCheeze <mon10180218@gmail.com>
2024-10-24 16:34:48 +0800

    add init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,2 @@
-vim.g.mapleader = " "
-
--- load lazy package manager
-require("lazy-setup")
-
--- load themes, options and mappings
-require("options")
-vim.schedule(function()
-	require("mappings")
-end)
+require("zhchay.core")
+-- require("zhchay.plugins")
