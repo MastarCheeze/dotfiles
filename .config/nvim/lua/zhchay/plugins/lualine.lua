eb57660778ef333b7ee7e06fde3e135e740ee622
MastarCheeze <mon10180218@gmail.com>
2024-10-24 21:57:15 +0800

    nvim plugin lualine

---
+++ .config/nvim/lua/zhchay/plugins/lualine.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = { "VeryLazy" },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      theme = "onedark",
+      globalstatus = true,
+      section_separators = "",
+      component_separators = "",
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { "filename" },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = {
+        {
+          "tabs",
+          show_modified_status = false,
+        },
+        "progress",
+      },
+      lualine_z = { "location" },
+    },
+  },
+}
